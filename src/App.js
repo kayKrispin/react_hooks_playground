7e10da91eef8dfb0d59922ad7850791d5864b97a
Taras Bashuryn <tarasbashuryn@Tarass-iMac.local>
2019-07-24 11:15:46 +0300

    reducer_hook

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import './App.css';
 
 function App() {
+
+  const reducer = (state, action) => {
+    switch (action.type) {
+      case "Increment" :
+        return state + 1;
+      case "Decrement" :
+        return Math.max(0,state - 1);
+      case "Reset" :
+        return state = 0
+    }
+  };
+
+  const [number, dispatch] = useReducer(reducer, 0)
+
   return (
     <div className="App">
-      Counter
-      <button>Increment</button>
-      <button>Decrement</button>
+      Counter: {number}
+      <button onClick={() => dispatch({ type: "Increment" })}>Increment</button>
+      <button onClick={() => dispatch({ type: "Decrement" })}>Decrement</button>
+      <button onClick={() => dispatch({ type: "Reset" })}>Reset</button>
     </div>
   );
 }
