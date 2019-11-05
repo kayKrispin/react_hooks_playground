9e21a35f262dc2034613c6f538765b5906f41971
Taras Bashuryn <tarasbashuryn@Tarass-iMac.local>
2019-11-05 13:14:56 +0200

    added_react_portal_&&_transitions

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
-import React, { useReducer } from 'react';
+import React, { useState, useReducer } from 'react';
+import ReactDOM from 'react-dom';
 import './App.css';
+import { CSSTransition } from 'react-transition-group';
+
 
 function App() {
 
@@ -15,6 +18,9 @@ function App() {
   };
 
   const [number, dispatch] = useReducer(reducer, 0)
+  const [showModal, toggleModal] = useState(false);
+  const [showBlock, toggleBlock] = useState(false);
+
 
   return (
     <div className="App">
@@ -22,8 +28,26 @@ function App() {
       <button onClick={() => dispatch({ type: "Increment" })}>Increment</button>
       <button onClick={() => dispatch({ type: "Decrement" })}>Decrement</button>
       <button onClick={() => dispatch({ type: "Reset" })}>Reset</button>
+      <button onClick={() => { toggleBlock(!showBlock) }} >show block</button>
+      <button onClick={() => {toggleModal(!showModal)}}>Show modal</button>
+
+              <Portal showModal={showModal} toggleModal={toggleModal} children={<div>hello there</div>} maxWidth={500}/>
     </div>
   );
 }
 
 export default App;
+
+
+function Portal ({ toggleModal, showModal, children, maxWidth }) {
+  return  ReactDOM.createPortal(
+    <CSSTransition  in={showModal} timeout={500} classNames="alert" mountOnEnter unmountOnExit>
+    <div className="my_modal" onClick={() => {toggleModal(!showModal)}}>
+        <div style={{width: maxWidth && maxWidth}} className="modal_body" onClick={e => e.stopPropagation()}>
+          {children}
+        </div>
+      </div>
+    </CSSTransition>
+    ,
+    document.body)
+}
